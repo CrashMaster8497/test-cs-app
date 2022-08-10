38aa3fe1b261903262431a96a883662ed0728ebd
CrashMaster8497 <crashmaster8497@gmail.com>
2022-08-11 00:06:41 +0800

    fix: add more tests

--- tests/Calculator.Tests/CalculatorTest.cs
+++ tests/Calculator.Tests/CalculatorTest.cs
@@ -25,20 +25,35 @@ namespace Calculator.Tests
         {
             Assert.Equal(2, Calculator.DoOperation(10, 5, "d"));
         }
+
         [Fact]
         public void ShouldDoDivision2()
         {
             Assert.Equal(2.4, Calculator.DoOperation(12, 5, "d"));
         }
+
         [Fact]
         public void ShouldDoDivision3()
         {
             Assert.Equal(12 / 3, Calculator.DoOperation(12, 3, "d"));
         }
+
         [Fact]
         public void ShouldNotDoDivision()
         {
             Assert.Equal(Double.NaN, Calculator.DoOperation(5, 0, "d"));
+        }
+
+        [Fact]
+        public void ShouldNotDoOperation1()
+        {
+            Assert.Equal(Double.NaN, Calculator.DoOperation(5, 7, ""));
+        }
+
+        [Fact]
+        public void ShouldNotDoOperation2()
+        {
+            Assert.Equal(Double.NaN, Calculator.DoOperation(5, 7, "as"));
         }
     }
 }
